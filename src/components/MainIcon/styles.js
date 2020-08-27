20476230ced40ef9557f32061fa68c75dbd333d8
abraao503 <abraao503@gmail.com>
2020-08-27 15:02:52 -0400

    create components folder

---
+++ src/components/MainIcon/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+`;
+
+export const Icon = styled.Image`
+  width: 140px;
+  height: 140px;
+  align-self: center;
+  margin-bottom: 10px;
+`;
+
+export const Title = styled.Text`
+  color: #ffdb58;
+  font-size: 20px;
+  text-transform: uppercase;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 0px;
+`;
