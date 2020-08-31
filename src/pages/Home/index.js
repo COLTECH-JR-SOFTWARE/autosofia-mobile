4d129bd0c8ad50b121999a25227d640218044171
evoney <evoney.mendonca@icomp.ufam.edu.br>
2020-08-31 14:11:22 -0400

    player components done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import { 
-  Container, 
-  Title, 
-  LogoContent, 
-  Message 
+import Player from '../../components/Player';
+
+import {
+  Container,
+  Title,
+  LogoContent,
+  Message
 } from './styles';
 import Background from '~/components/Background';
 import MainIcon from '~/components/MainIcon';
@@ -13,7 +15,8 @@ const Home = () => {
   return (
     <Background blurRadius={0}>
       <Container>
-        <MainIcon title="AUTOSOFIA"/>      
+        <MainIcon title="AUTOSOFIA"/>
+        <Player/>
         <Message>Bem vindo, User</Message>
       </Container>
     </Background>
